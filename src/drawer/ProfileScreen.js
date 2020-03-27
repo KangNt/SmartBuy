b676d36eaab1047df4ca3a8c965b07c8725dee24
QuangNT <quangnt@novaon.vn>
2020-03-27 11:13:53 +0700

    Screen Profile

--- src/drawer/ProfileScreen.js
+++ src/drawer/ProfileScreen.js
@@ -5,42 +5,76 @@ import {
     Text,
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView
 } from 'react-native';
 import { CustomHeader } from '../index'
+
+import {FontAwesome5 }from '@expo/vector-icons'
+
 export class ProfileScreen extends Component {
 
     render() {
         return (
+            
             <View style={styles.container}>
-                <CustomHeader title="Profile" navigation={this.props.navigation} />
+                <CustomHeader title="Profile" navigation={this.props.navigation}  />
                 <View style={styles.header}>
                 </View>
                 
                 <Image style={styles.avatar} source={IMAGE.ICON_PROFILE} />
+                <ScrollView>
                 <View style={styles.body}>
+                    {/* <Text style={{ fontSize: 22,color: "#696969",}}>KidPlaza</Text> */}
+                
                     <View style={styles.bodyContent}>
-                        <Text style={styles.name}>John Doe</Text>
+                        {/* <Text style={styles.name}>John Doe</Text>
                         <Text style={styles.info}>UX Designer / Mobile developer</Text>
-                        <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
-
+                        <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text> */}
+                        
                         <TouchableOpacity style={styles.buttonContainer}>
-                            <Text>Opcion 1</Text>
+                            <Text><FontAwesome5 name="user" size={20} /> Giới tính </Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.buttonContainer}>
-                            <Text>Opcion 2</Text>
+                        <Text><FontAwesome5 name="mobile-alt" size={20} /> Số điện thoại</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonContainer}>
+                        <Text><FontAwesome5 name="birthday-cake" size={20} /> Ngày sinh</Text>
+                        </TouchableOpacity>
+                        
+                    </View>
+                    <View style={styles.bodyContent}>
+                        <TouchableOpacity style={styles.buttonContainers}>
+                        <Text><FontAwesome5 name="envelope" size={20} /> Email</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonContainers}>
+                        <Text><FontAwesome5 name="map-marker-alt" size={20} /> Địa chỉ</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonContainers}>
+                        <Text><FontAwesome5 name="calendar-alt" size={20} /> Ngày tham gia</Text>
                         </TouchableOpacity>
                     </View>
+                        
+                    
                 </View>
+                <View style={styles.bodyContent}>
+                        <TouchableOpacity style={styles.buttonContainerss}>
+                        <Text><FontAwesome5 name="phone" size={20} />   Danh bạ    </Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
             </View>
+            
         );
     }
 }
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: "#00BFFF",
+        backfaceVisibility:'visible',
         height: 200,
+        backgroundColor:"#AE87E6"
+        
     },
     avatar: {
         width: 130,
@@ -60,11 +94,13 @@ const styles = StyleSheet.create({
     },
     body: {
         marginTop: 40,
+        flexDirection:'row'
     },
     bodyContent: {
         flex: 1,
         alignItems: 'center',
         padding: 30,
+       
     },
     name: {
         fontSize: 28,
@@ -89,10 +125,36 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 20,
+        width: 150,
+        borderRadius: 30,
+        backgroundColor: "#FEF9F3",
+        marginLeft:-10
+    },
+    buttonContainers: {
+        marginTop: 10,
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 150,
+        borderRadius: 30,
+        backgroundColor: "#FEF9F3",
+        
+    },
+    buttonContainerss: {
+        marginTop: -30,
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
         width: 250,
         borderRadius: 30,
-        backgroundColor: "#00BFFF",
+        backgroundColor: "#FEF9F3",
+        
     },
+
 });
 
 
